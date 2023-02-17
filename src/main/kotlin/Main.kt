56b5c7c90b5a5efc5cf6eec7a4c6b10012f72b75
Your Name <you@example.com>
2023-02-17 10:47:17 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,22 @@
+fun main(args: Array<String>) {
+    val school = "Akirachix"
+    println(school[0]+"" +school[2]+"" +school[3])
+
+    var x = "Marrion"
+    var y = 21
+    println("Hi,my name is $x and I am $y years old")
+
+    var d = "size"
+    println(d.length)
+
+    val name = "Marrion"
+    if (name=="Marrion") {
+        println("Thats me")
+    } else{
+        println("Idont know who that is")
+    }
+
+
+
+
+}
